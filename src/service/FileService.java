d6833e1b5534e014b9f142940282c962fc0db714
Jorko2000 <george1102@abv.bg>
2026-04-23 03:46:50 +0300

    FileService.java

--- src/service/FileService.java
+++ src/service/FileService.java
@@ -1,30 +1,60 @@
-public static List<Car> readFromCSV(String path) {
-    List<Car> cars = new ArrayList<>();
+package service;
 
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-        String line;
+import model.Car;
+import java.io.*;
+import java.util.*;
 
-        while ((line = br.readLine()) != null) {
-            String[] data = line.split(",");
+public class FileService {
 
-            String renter = data.length > 6 ? data[6] : "";
+    // READ CSV
+    public static List<Car> readFromCSV(String path) {
+        List<Car> cars = new ArrayList<>();
 
-            Car car = new Car(
-                    data[0],
-                    data[1],
-                    data[2],
-                    Integer.parseInt(data[3]),
-                    data[4],
-                    data[5],
-                    renter
-            );
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
 
-            cars.add(car);
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String renter = data.length > 6 ? data[6] : "";
+
+                Car car = new Car(
+                        data[0],
+                        data[1],
+                        data[2],
+                        Integer.parseInt(data[3]),
+                        data[4],
+                        data[5],
+                        renter
+                );
+
+                cars.add(car);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + e.getMessage());
         }
 
-    } catch (Exception e) {
-        System.out.println("Error reading file: " + e.getMessage());
+        return cars;
     }
 
-    return cars;
+    // WRITE CSV
+    public static void writeToCSV(String path, List<Car> cars) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+
+            for (Car car : cars) {
+                bw.write(car.getId() + "," +
+                        car.getMake() + "," +
+                        car.getModel() + "," +
+                        car.getYear() + "," +
+                        car.getType() + "," +
+                        car.getStatus() + "," +
+                        (car.getCurrentRenter() == null ? "" : car.getCurrentRenter()));
+                bw.newLine();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+    }
 }
